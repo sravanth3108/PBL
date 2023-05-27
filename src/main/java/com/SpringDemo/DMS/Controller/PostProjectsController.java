7a2ef5c8c2bde69253f499139533499af0d0b8f1
Pullamgari <S556513@nwmissouri.edu>
2023-05-26 19:48:57 -0500

    displaying project after on click

--- src/main/java/com/SpringDemo/DMS/Controller/PostProjectsController.java
+++ src/main/java/com/SpringDemo/DMS/Controller/PostProjectsController.java
@@ -2,27 +2,21 @@ package com.SpringDemo.DMS.Controller;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.servlet.ModelAndView;
 import com.SpringDemo.DMS.Model.PostProjects;
 import com.SpringDemo.DMS.Repository.PostProjectsRepository;
 
 import jakarta.servlet.http.HttpSession;
 
-@Controller
+@RestController
 public class PostProjectsController {
     @Autowired
     private PostProjectsRepository projectRepo;
@@ -52,16 +46,34 @@ public class PostProjectsController {
     }
     
     @GetMapping("/viewProjects")
-    public  String viewProjects(Model model) {
+    public  ModelAndView viewProjects(Model model) {
         List<PostProjects> project = projectRepo.findAll();
        
             model.addAttribute("projects", project);
+            System.out.println(project);
 
-       
-        return "view-Projects";
+            ModelAndView modelAndView = new ModelAndView();
+            modelAndView.setViewName("view-projects.html"); // Redirect to the viewprojects endpoint
+            return modelAndView;
     }
     
     
+    @GetMapping("/projectDetails")
+    public ModelAndView getProjectDetails(@RequestParam("name") String projectName, Model model) 
+    {
+    	PostProjects project = projectRepo.findById(projectName).orElse(null);
+    	String projDesc = project.getProjDesc();
+	    model.addAttribute("projectName", projectName);
+	   model.addAttribute("projDesc",projDesc);
+	    System.out.println(project);
+	    ModelAndView modelAndView = new ModelAndView();
+	    modelAndView.setViewName("projDetails");
+	    return modelAndView;
+  
+    }
+    
+    
+    /*
     @PostMapping("/projectDetails")
     public void handleProjectName(@RequestBody Map<String, String> payload, HttpSession session) {
         projectName = payload.get("projectName");
@@ -86,7 +98,7 @@ public class PostProjectsController {
     	    ModelAndView modelAndView = new ModelAndView();
     	    modelAndView.setViewName("projDetails");
     	    return modelAndView;
-       }
+       }*/
 
         
     }
